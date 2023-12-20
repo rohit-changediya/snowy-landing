09cf487a57ce8e84a7498978a38dac64668aa06c
developer devo <rohitchangediya@gmail.com>
2023-12-20 18:37:32 +0530

    initial commit

--- index.php
+++ index.php
@@ -11,7 +11,7 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     <style>
     body {
-    background-color: #4D9541;
+    background-color: #0ECAF5;
     font-family: 'Silkscreen', sans-serif;
     font-weight: 400;
     font-size: 1.25rem;
