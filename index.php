f8a182f6bfae053a87c218b04bb592cf4eade8ef
rizwansweb <rizwansweb@gmail.com>
2023-12-21 11:40:41 +0530

    Add files via upload

--- index.php
+++ index.php
@@ -3,15 +3,19 @@
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Struppi</title>
+    <title>Injective Snowy</title>
     <link rel="icon" type="image/x-icon" href="images/favicon.ico">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Silkscreen:wght@400;700&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.2/font/bootstrap-icons.min.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     <style>
     body {
-    background-color: #0ECAF5;
+    background-color: #000;
+    background-image: url('images/bg.gif');
+    background-size: cover;
+    background-repeat: no-repeat;
     font-family: 'Silkscreen', sans-serif;
     font-weight: 400;
     font-size: 1.25rem;
@@ -20,30 +24,63 @@
     small {
         font-size: 0.8rem;
     }
+    .bg-black {
+      position: fixed;
+      width: 100%;
+      height: 100%;
+      background: #000;
+      opacity: 0.5;
+      z-index: -1;
+    }
     </style>
   </head>
   <body>
+    <div class="bg-black"></div>
     <div class="container text-white text-center py-5">
       <div class="row justify-content-center">
         <div class="col-md-12">
-          <h2>$KIMBO</h2>
-          <h4>KIMBO is COQ's best friend</h4>
+          <h1>Welcome to Injective Snowy</h1>
+          <h4>The biggest Injective Advanture Airdrop!</h4>
         </div>
-        <div class="col-md-5 py-4">
+        <div class="col-7 col-md-3 py-4">
           <img src="images/struppi.png" class="img-fluid">
         </div>
         <div class="col-md-12">
-          <p class="mb-0">0 Team allocation. taxes or presale.</p>
-          <p class="mb-4">0 Team allocation. taxes or presale.</p>
-          <p class="mb-0">69,420,000,000.00, total $kimbo</p>
-          <p class="mb-0">Contract: 0x184ff13B3EBCB25Be44e860163A5D8391Dd568c1</p>
-          <p class="mb-0">Contract renounced</p>
-          <p class="mb-0">Liquidity Locked</p>
-          <p class="mb-0"><a href="#" class="text-decoration-none text-white">BUY $KIMBO</a></p>
-          <p class="mb-0"><a href="#" class="text-decoration-none text-white">Dexscreener</a></p>
-          <p class="mb-0"><a href="#" class="text-decoration-none text-white">Twitter</a></p>
-          <p class="mb-5"><a href="#" class="text-decoration-none text-white">Telegram</a></p>
-          <p><small>$KIMBO is a meme coin with no intrinsic value or expectation of financial return. There is no formal team or roadmap. The coin is completely useless and for entertainment purposes only.</small></p>
+          <a href="https://t.me/InjSnowy_bot" class="btn btn-light btn-lg mb-2">Airdrop Bot</a>
+          <p class="mb-4">Join the Fun with Snowy Token on the Injective Network! 🚀❄️</p>
+          <h4>Who We Are:</h4>
+          <p class="mb-4">Welcome to the whimsical world of Snowy Token, the latest meme sensation on the Injective Network. Inspired by Tintin's intrepid and loyal companion, Snowy Token embodies the spirit of adventure and the joy of exploration in the crypto universe.</p>
+          <p class="mb-4">🐾 Join us now and be part of the Snowy Token saga on the Injective Network! 🐾</p>
+          <div class="row align-items-center justify-content-center mb-4">
+            <div class="col-6 col-md-auto mb-3 mb-md-0">
+              <div class="d-grid">
+                <a href="https://discord.gg/4rKDGfDh72" class="btn btn-outline-light"><i class="bi bi-discord"></i> Discord</a>
+              </div>
+            </div>
+            <div class="col-6 col-md-auto mb-3 mb-md-0">
+              <div class="d-grid">
+                <a href="https://t.me/injsnowy" class="btn btn-outline-light"><i class="bi bi-telegram"></i> Telegram</a>
+              </div>
+            </div>
+            <div class="col-6 col-md-1 mb-3 mb-md-0">
+              <div class="d-grid">
+                <a href="https://twitter.com/InjSnowy" class="btn btn-outline-light"><i class="bi bi-twitter-x"></i> X</a>
+              </div>
+            </div>
+            <div class="col-6 col-md-auto mb-3 mb-md-0">
+              <div class="d-grid">
+                <a href="https://medium.com/@injsnowy" class="btn btn-outline-light"><i class="bi bi-medium"></i> Medium</a>
+              </div>
+            </div>
+            <div class="col-auto col-md-auto mb-3 mb-md-0">
+              <div class="d-grid">
+                <a href="https://github.com/InjSnowy" class="btn btn-outline-light"><i class="bi bi-github"></i> Github</a>
+              </div>
+            </div>
+          </div>
+          <p class="mb-4">E-mail: <a href="mailto:hello@injsnowy.com" class="text-decoration-none text-white">hello@injsnowy.com</a></p>
+          <p class="mb-5"><a href="https://explorer.injective.network/asset/?tokenType=tokenFactory&denom=factory/inj1ml33x7lkxk6x2x95d3alw4h84evlcdz2gnehmk/SNOWY" class="text-decoration-none text-white">Token Contract</a></p>
+          <p class="lh-1"><small>Disclaimer: The information provided on this website is for general informational purposes only. It is not intended as financial, legal, or investment advice and should not be construed or relied upon as such. We do not accept any responsibility for any loss which may arise from reliance on information contained on this site. Always consult a professional advisor before making any financial decisions.</small></p>
         </div>
       </div>
     </div>
